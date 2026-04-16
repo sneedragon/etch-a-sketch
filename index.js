4e76f018b40abcff19649e0cab9aab2ba5d1f96f
Charles Sneedworth <sneedworth@sevensevenseven.net>
2026-04-16 20:52:18 +0200

    Create parseColor function to extract square color

--- index.js
+++ index.js
@@ -19,9 +19,21 @@ function createGrid() {
     }
 };
 
+function parseColor(string) {
+
+    let colorArr = string.slice(4, -1).split(",");
+    return colorArr;
+};
+
+function darkenSquare(colors) {
+    //TODO: lower values and modify
+}
+
 grid.addEventListener("mouseover", (event) => {
     let target = event.target;
-    console.log(target);
+    //TODO: check if class = square
+    //darkenSquare(parseColor(getComputedStyle(target.backgroundColor)))
+    //parseColor(getComputedStyle(target).backgroundColor);
 });
 
 
